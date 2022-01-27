7cdaee28b32a440bb3656e512d8c1300c58b8703
joao.garcia <joao.vfg@hotmail.com.br>
2022-01-27 10:01:36 -0300

    "Inserindo mais testes e implementando HashCode e Equals na classe Publicacao"

--- src/main/java/JoaoVFG/com/github/DesafioOystr/Entity/Publicacao.java
+++ src/main/java/JoaoVFG/com/github/DesafioOystr/Entity/Publicacao.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -50,5 +51,18 @@ public class Publicacao {
                 ", processo='" + processo + '\'' +
                 ", chaveUnica='" + chaveUnica + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publicacao that = (Publicacao) o;
+        return id.equals(that.id) && data.equals(that.data) && evento.equals(that.evento) && texto.equals(that.texto) && processo.equals(that.processo) && chaveUnica.equals(that.chaveUnica);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, data, evento, texto, processo, chaveUnica);
     }
 }
--- src/test/java/JoaoVFG/com/github/DesafioOystr/DesafioOystrApplicationTests.java
+++ src/test/java/JoaoVFG/com/github/DesafioOystr/DesafioOystrApplicationTests.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
@@ -38,10 +39,10 @@ public class DesafioOystrApplicationTests {
 	@Before
 	public void inicializacaoDeObjetos() {
 		publicacaoDTO1 = new PublicacaoDTO("22/12/2022", "0000002-31.2010.5.01.001", "Ut sit amet nisl id leo eleifend dignissim. Curabitur eu pellentesque sem. Mauris quis condimentum tortor. Curabitur augue arcu, fermentum vel luctus et, consectetur laoreet nibh. Fusce volutpat diam at risus ultrices, sit amet aliquet lorem viverra. Morbi id ex sem. Donec varius scelerisque sem a aliquet. Nam tristique accumsan diam, tempor tristique magna sodales vel. Aliquam quis feugiat mi, nec tempus est. Duis metus nibh, pretium ac ullamcorper et, imperdiet ut mi. Nullam nunc purus, congue quis velit eget, scelerisque congue nisl. Mauris id mattis odio. Curabitur eu leo eu velit elementum mattis eget in mauris.","DESPACHO");
-		publicacaoDTO2 = new PublicacaoDTO("21/07/2022 17:00:00", "0000002-31.2010.5.01.001","123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123","DESPACHO");
-		publicacaoDTO3 = new PublicacaoDTO("24/06/2022 09:51:00", "0000002-31.2010.5.01.001", "TEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTO","DESPACHO");
-		publicacaoDTO4 = new PublicacaoDTO("10/01/2022", "0000002-31.2010.5.01.001", "T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3","DESPACHO");
-		publicacaoDTO5 = new PublicacaoDTO("04/09/2022 04:30:00", "0000002-31.2010.5.01.001","TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%","DESPACHO");
+		publicacaoDTO2 = new PublicacaoDTO("21/07/2022 17:00:00", "0009902-07.2020.5.10.002","123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123","DESPACHO");
+		publicacaoDTO3 = new PublicacaoDTO("24/06/2022 09:51:00", "1236547-06.2013.5.03.005", "TEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTO","DESPACHO");
+		publicacaoDTO4 = new PublicacaoDTO("10/01/2022", "0020012-10.2015.5.05.007", "T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3T3ST3","DESPACHO");
+		publicacaoDTO5 = new PublicacaoDTO("04/09/2022 04:30:00", "3457894-21.2011.5.06.009","TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%TEXTO!@#$%","DESPACHO");
 	}
 
 	@Test
@@ -61,7 +62,7 @@ public class DesafioOystrApplicationTests {
 
 	@Test
 	public void teste03_PesquisaPorChaveUnica() {
-		Publicacao publicacao = publicacaoService.findByChaveUnica("DACE6EEB5723A14EF1B0165430CDAFA8");
+		Publicacao publicacao = publicacaoService.findByChaveUnica("B040823F78CFB45FF930B43132B82B71");
 		assertNotNull(publicacao);
 	}
 
@@ -79,7 +80,25 @@ public class DesafioOystrApplicationTests {
 
 	}
 
+	@Test
+	public void teste05_QuantidadeRetornada() {
+		List<Publicacao> publicacoes = publicacaoService.findAll();
+		assertEquals(publicacoes.size(),5);
+	}
 
+	@Test
+	public void teste06_CompararConteudoTexto() {
+		Publicacao publicacao = publicacaoService.findByChaveUnica("AA20E07053B590D2EED05E8342F0133B");
+		assertEquals(publicacao.getTexto(),"TEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTOTEXTO");
+	}
+
+	@Test
+	public void teste07_ObjetoCriadoComRetornado() {
+		PublicacaoDTO publicacaoDTO6 = new PublicacaoDTO("22/12/2022 08:00:00", "0030045-22.2018.2.05.009", "TesteTexto123TesteTexto123TesteTexto123TesteTexto123TesteTexto123TesteTexto123TesteTexto123TesteTexto123TesteTexto123TesteTexto123TesteTexto123","DESPACHO");
+		Publicacao publicao = publicacaoService.create(publicacaoDTO6);
+		Publicacao buscaPublicacao = publicacaoService.findById(6);
+		assertEquals(publicao,buscaPublicacao);
+	}
 
 
 
